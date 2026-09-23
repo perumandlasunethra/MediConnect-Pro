e6bcb2950cb1491732ead4ca076cc5f4b46551b3
perumandlasunethra <perumandlasunethra@gmail.com>
2026-09-23 19:30:13 +0530

    Connect MediConnect Pro to Aiven database

--- src/main/java/com/example/mediconnect/dao/DBConnection.java
+++ src/main/java/com/example/mediconnect/dao/DBConnection.java
@@ -9,10 +9,21 @@ public class DBConnection {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
+        String host = System.getenv("DB_HOST");
+        String port = System.getenv("DB_PORT");
+        String database = System.getenv("DB_NAME");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        String url =
+            "jdbc:mysql://" + host + ":" + port + "/" + database +
+            "?sslMode=REQUIRED&serverTimezone=UTC";
+
         return DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/mediconnect",
-            "root",
-            ""
+            url,
+            user,
+            password
         );
     }
 }
+
